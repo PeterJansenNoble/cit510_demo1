58a0c251a61625ffdd79ed12e64dd6e6d45b0f20
PeterJansenNoble <70250107+PeterJansenNoble@users.noreply.github.com>
2022-09-09 19:44:42 +0800

    update modified

--- src/views/About.vue
+++ src/views/About.vue
@@ -130,7 +130,7 @@ section > h1 {
 
 .masthead {
   padding: 6em 0;
-  background-image: url('https://i.imgur.com/yGmrlyg.jpg');
+  
   background-size: cover;
   background-repeat: no-repeat;
   border-top: solid 1em #1d2920;
